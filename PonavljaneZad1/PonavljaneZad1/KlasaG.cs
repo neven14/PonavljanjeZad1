de8fe992c081f7143caab1da4c737ee3d531a72e
Neven <58472164+neven14@users.noreply.github.com>
2021-06-13 09:05:46 +0200

    Add files via upload

---
+++ PonavljaneZad1/PonavljaneZad1/KlasaG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljaneZad1
+{
+    class KlasaG
+    {
+		public void Plus10(int Broj)
+		{
+			Broj = Broj + 10;
+			Console.WriteLine("Plus10 od unesenog broja = " + Broj);
+		}
+		public void f50(int Broj)
+		{
+			Broj = 50;
+			Console.WriteLine("f50 od unesenog broja = " + Broj);
+		}
+	}
+}
---
+++ PonavljaneZad1/PonavljaneZad1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljaneZad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaG MainObjekt = new KlasaG();
+            int Broj;
+            Console.Write("Upisite broj: ");
+            Broj = Convert.ToInt32(Console.ReadLine());
+            MainObjekt.Plus10(Broj);
+            MainObjekt.f50(Broj);
+
+            Console.ReadKey();
+        }
+    }
+}
